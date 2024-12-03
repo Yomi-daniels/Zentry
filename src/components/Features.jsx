aff173bd8db286bceea954aac13a258badb38e63
YODA <121935558+Yomi-daniels@users.noreply.github.com>
2024-12-03 16:03:33 +0100

    updating changes

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { TiLocationArrow } from "react-icons/ti";
 
 const BentoCard = ({
@@ -32,6 +32,42 @@ const BentoCard = ({
   );
 };
 
+const BentoTilt = ({
+  children, className = ''
+}) => {
+  const [transformStyle, setTransformStyle] = useState('')
+  const itemRef = useRef()
+
+  const handleMouseMove = (e) => {
+    if(!itemRef.current) return
+
+    const {left, top, width, height} = itemRef.current.getBoundingClientRect(); 
+
+    const relativeX = (e.clientX - left) / width;
+    const relativeY = (e.clientY - top) / height;
+
+    const tiltX = (relativeY - 0.5) * 5
+    const tiltY = (relativeX - 0.5) * -5
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.98, 0.98, 0.98)`
+
+    setTransformStyle(newTransform)
+  }
+
+  const handleMouseLeave = () => {
+    setTransformStyle('')
+  }
+  return (
+    <div className={className}
+    ref={itemRef}
+    onMouseMove={handleMouseMove}
+    onMouseLeave={handleMouseLeave}
+    style={{transform: transformStyle}}>
+      {children}
+    </div>
+  )
+}
+
 const Features = () => {
   return (
     <section className="bg-black pb-52">
@@ -47,7 +83,7 @@ const Features = () => {
           </p>
         </div>
 
-        <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+        <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
           <BentoCard
             src="videos/feature-1.mp4"
             title={
@@ -58,9 +94,9 @@ const Features = () => {
             description="A cross-platform metagame app, turning your activities across web2 and web3 games into a rewarding adventure."
             titleGradient="from-red-500 to-yellow-500"
           />
-        </div>
+        </BentoTilt>
         <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
-          <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+          <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
             <BentoCard
               src="videos/feature-2.mp4"
               title={
@@ -71,8 +107,8 @@ const Features = () => {
               description="An anime and gaming-inspired collection - the IP primed for expansion."
               titleGradient="from-blue-500 to-green-500"
             />
-          </div>
-          <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
             <BentoCard
               src="videos/feature-3.mp4"
               title={
@@ -83,8 +119,8 @@ const Features = () => {
               description="A gamified social hub, adding a new dimension of play to social interaction for web3 communities."
               titleGradient="from-purple-500 to-pink-500"
             />
-          </div>
-          <div className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
             <BentoCard
               src="videos/feature-4.mp4"
               title={
@@ -95,15 +131,15 @@ const Features = () => {
               description="A cross-world AI Agent - elevating your gameplay to be more fun and protective."
               titleGradient="from-yellow-500 to-orange-500"
             />
-          </div>
-                <div className="bento-tilt_2">
+          </BentoTilt>
+                <BentoTilt className="bento-tilt_2">
                     <div className="flex size-full flex-col justify-between bg-violet-300 p-5 ">
                         <h1 className="bento-title special-font max-w-64">M<b>o</b>re co<b>m</b>ing s<b>o</b>on!
                    
                         </h1>
                         <TiLocationArrow className="m-5 scale-[5] self-end"/>
                     </div>
-                </div>
+                </BentoTilt>
                 <div className="bento-tilt_2">
                 <video src="videos/feature-5.mp4"
                     loop
